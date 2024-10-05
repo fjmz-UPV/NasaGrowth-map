411e6bba5a11e368ca1e1a4b5c497eb7f1a1d8ca
d0vak1n <11877941+d0vak1n@users.noreply.github.com>
2024-10-05 17:28:43 +0000

    feat: setted rotating and several paths

--- src/main.js
+++ src/main.js
@@ -3,28 +3,62 @@ import {
   Math as CesiumMath,
   Terrain,
   Viewer,
-  createOsmBuildingsAsync,
+  ModelGraphics
 } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "./style.css";
+import { polygonEntityAitAmira, polygonEntityFerkes } from "./locations";
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 const viewer = new Viewer("cesiumContainer", {
   terrain: Terrain.fromWorldTerrain(),
 });
 
-// Fly the camera to San Francisco at the given longitude, latitude, and height.
-/*
+// Fly the camera to XXXXX at the given longitude, latitude, and height.
+const flyToFirstPoint = () => {
 viewer.camera.flyTo({
-  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+  destination: Cartesian3.fromDegrees(-9.433781, 30.143166, 3000),
   orientation: {
     heading: CesiumMath.toRadians(0.0),
-    pitch: CesiumMath.toRadians(-15.0),
+    pitch: CesiumMath.toRadians(-90.0),
   },
 });
-*/
+};
 
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-createOsmBuildingsAsync().then((buildingTileset) => {
-  viewer.scene.primitives.add(buildingTileset);
-});
+// Fly the camera to the second point
+const flyToSecondPoint = () => {
+  viewer.camera.flyTo({
+    destination: Cartesian3.fromDegrees(-5.131704, 9.641149, 3000),
+    orientation: {
+      heading: CesiumMath.toRadians(0.01),
+      pitch: CesiumMath.toRadians(-90.0),
+    },
+  });
+};
+
+// Function to rotate the Earth
+const rotateEarth = () => {
+  const currentTime = viewer.clock.currentTime;
+  const rotationSpeed = CesiumMath.toRadians(0.01); // Adjust the rotation speed
+  viewer.scene.camera.rotate(Cartesian3.UNIT_Z, rotationSpeed);
+};
+
+// Function to stop rotating the Earth
+const stopRotatingEarth = () => {
+  viewer.scene.postRender.removeEventListener(rotateEarth);
+};
+
+// EXECUTING THE CODE
+
+// Add the polygon entity to the viewer
+viewer.entities.add(polygonEntityAitAmira);
+viewer.entities.add(polygonEntityFerkes);
+
+// Add the rotateEarth function to the postRender event
+viewer.scene.postRender.addEventListener(rotateEarth);
+
+setTimeout(() => {
+  stopRotatingEarth();
+  flyToFirstPoint();
+  setTimeout(flyToSecondPoint, 5000);
+}, 8000);
