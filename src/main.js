90c3f6ffe6355f25cff7ec81b9d449ab391b1095
d0vak1n <11877941+d0vak1n@users.noreply.github.com>
2024-10-06 10:50:14 +0000

    fix: location fixed

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const viewer = new Viewer("cesiumContainer", {
 // Fly the camera to XXXXX at the given longitude, latitude, and height.
 const flyToAitAmira = () => {
 viewer.camera.flyTo({
-  destination: Cartesian3.fromDegrees(-9.433781, 30.143166, 2000),
+  destination: Cartesian3.fromDegrees(-9.433781, 30.138166, 2000),
   orientation: {
     heading: CesiumMath.toRadians(0.0),
     pitch: CesiumMath.toRadians(-90.0),
@@ -27,7 +27,7 @@ viewer.camera.flyTo({
 // Fly the camera to the second point
 const flyToFerkes = () => {
   viewer.camera.flyTo({
-    destination: Cartesian3.fromDegrees(-5.131704, 9.641149, 2000),
+    destination: Cartesian3.fromDegrees(-5.131704, 9.635149, 2000),
     orientation: {
       heading: CesiumMath.toRadians(0.01),
       pitch: CesiumMath.toRadians(-90.0),
@@ -37,7 +37,7 @@ const flyToFerkes = () => {
 
 const flyToPeru = () => {
   viewer.camera.flyTo({
-    destination: Cartesian3.fromDegrees(-79.922369, -6.102769, 2000),
+    destination: Cartesian3.fromDegrees(-79.922369, -6.105769, 2000),
     orientation: {
       heading: CesiumMath.toRadians(0.01),
       pitch: CesiumMath.toRadians(-90.0),
@@ -47,7 +47,7 @@ const flyToPeru = () => {
 
 const flyToThai = () => {
   viewer.camera.flyTo({
-    destination: Cartesian3.fromDegrees(101.328928, 15.135729, 2000),
+    destination: Cartesian3.fromDegrees(101.328928, 15.130729, 2000),
     orientation: {
       heading: CesiumMath.toRadians(0.01),
       pitch: CesiumMath.toRadians(-90.0),
